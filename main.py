2962c47f02cab85855cf8b9ff07e6ebc014257e3
itodaisuke <itodaisuke0812@gmail.com>
2020-12-16 00:44:16 +0900

    creating chat

--- main.py
+++ main.py
@@ -1,15 +1,63 @@
 # -*- coding:utf-8 -*-
 import sys
-from flask import Flask
+from flask import Flask,request,jsonify
 
-#import MeCab
+from gevent.pywsgi import WSGIServer
+from geventwebsocket.handler import WebSocketHandler
+
+from flask_restful import Resource, Api, marshal_with
+from flask_socketio import SocketIO,emit,send
+#import bbbserial as bbb
+from threading import Thread
+import time
+
+async_mode = None
+
+# Flaskオブジェクトを生成し、セッション情報暗号化のキーを指定
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+
+# Flaskオブジェクト、async_modeを指定して、SocketIOサーバオブジェクトを生成
+socketio = SocketIO(app, async_mode=async_mode)
 
 app = Flask(__name__, static_url_path='', static_folder='./dist/myweb')
 app.config['JSON_AS_ASCII'] = False
 
+
+
 @app.route('/', methods=['GET'])
 def getAngular():
+    #print(request.args)
+    #print(request.environ)
+    if request.environ.get("wsgi.websocket"):
+        print("connected!!")
     return app.send_static_file('index.html')
+#print(request.environ)
+"""@app.route("/<name>",methods=['GET'])
+def namepage(name):
+    print("name",name)
+    return app.send_static_file("index.html")"""
+
+@socketio.on('connect', namespace='/chat')
+def test_connect():
+    print('Client connected')
+    return "poke"
+
 
 if __name__ == '__main__':
-    app.run()
+    app.debug = True
+    server = WSGIServer(('localhost', 5000), app, handler_class=WebSocketHandler)
+    server.serve_forever()
+    #app.run(port="5000",host="localhost")
+    #socket.run(app,debug=True)
+    print("app running")
+    """host = "localhost"
+    port = 8080
+
+    host_port = (host, port)
+    server = WSGIServer(
+       host_port,
+       app,
+       handler_class=WebSocketHandler
+    )
+    server.serve_forever()"""
--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,17 +1,67 @@
 import {AfterViewInit, Component, ElementRef, ViewChild,OnInit} from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-//import { Observable } from 'rxjs/Observable';
-//var util  = require('util');
-//var spawn = require('child_process').spawn;
-//var php   = spawn('php', ['callme.php']);
-class send_query {
-  public send_1(params: string){
-    console.log(params)
+
+import {Subject, Observable, Observer} from 'rxjs/Rx';
+import {Injectable} from '@angular/core';
+
+import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
+export class ChatService {
+
+    serve(): WebSocketSubject<MessageEvent> {
+      console.log("suzu")
+      return webSocket({
+        url: `ws://localhost:5000/chat`,
+        deserializer: message => {
+          console.log("this is a message")
+          console.log(message.data)
+
+          return message;
+        }
+      });
+    }
+
+  constructor() { }
+}
+
+@Injectable()
+export class WebSocketService {
+  private subject: Subject<MessageEvent>;   // -- ① WebSocket接続時に返す Subject
+
+  connect(url: string): Subject<MessageEvent> {   // -- ②  このメソッドを呼び出して Subject を手に入れます
+    if (!this.subject) {
+      this.subject = this.create(url);
+    }
+    return this.subject;
+  }
+
+  private create(url: string): Subject<MessageEvent> {
+    const ws = new WebSocket(url);　　// -- ③ WebSocket オブジェクトを生成します
+
+    const observable = Observable.create((obs: Observer<MessageEvent>) => { // -- ④ Observable オブジェクトを生成します
+      ws.onmessage = obs.next.bind(obs);
+      ws.onerror = obs.error.bind(obs);
+      ws.onclose = obs.complete.bind(obs);
+
+      return ws.close.bind(ws);
+    });
+
+    const observer = {    // -- ⑤ Observer オブジェクトを生成します
+      next: (data: Object) => {
+        if (ws.readyState === WebSocket.OPEN) {
+          ws.send(JSON.stringify(data));
+        }
+      },
+    };
+    return Subject.create(observer, observable);    // -- ⑥ Subject を生成してconnect
   }
 }
 
-let sss = new send_query
-sss.send_1("k")
 
 @Component({
   selector: 'app-chat',
@@ -19,23 +69,28 @@ sss.send_1("k")
   styleUrls: ['./chat.component.css']
 })
 
-
 export class ChatComponent implements OnInit {
+  subject: WebSocketSubject<MessageEvent>
 
   constructor() {}
   @ViewChild("test") test: ElementRef
-  @ViewChild("bird") bird: ElementRef
   ngOnInit(){
+    console.log("initialized")
+    let chat : ChatService = new ChatService
+    this.subject = chat.serve();
+    //console.log(this.subject)
+    console.log("next")
+    this.subject.subscribe(
+      (message: MessageEvent) => {
+        message = JSON.parse(message.data);
+        console.log("pokemon")
+      })
   }
   ngAfterViewInit() {
-    console.log(this.test.nativeElement.innerHTML)
-    //this.test.nativeElement.innerHTML = "こんばんは"
-    console.log(this.bird.nativeElement)
+    var text  = this.test.nativeElement.innerHTML
+    const fun1 = (pokemon: string) =>{
+    }
+    console.log("")
+    this.test.nativeElement.onclick = fun1(text)
   }
 }
-
-
-
-
-
-//console.log(this.test.nativeElement.innerHTML);
