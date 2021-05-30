4d3e7f12ba622af397a75c754af6a385a18c5b58
itodaisuke <itodaisuke0812@gmail.com>
2021-05-30 17:12:34 +0900

    prod commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ChatComponent } from './chat/chat.component';
 import { MainComponent } from './main/main.component';
-import { LoginComponent} from "./login/login.component"
-import { StopwatchComponent} from "./stopwatch/stopwatch.component"
+import { LoginComponent} from "./login/login.component";
+import { StopwatchComponent} from "./stopwatch/stopwatch.component";
+import { OthersComponent } from './others/others.component';
 
 const myRoutes: Routes = [
   //{path:"",component: AppComponent},
   { path: '', component: MainComponent },
   { path: 'chat', component: ChatComponent},
   { path: 'login', component: LoginComponent},
-  { path: 'stopwatch', component: StopwatchComponent}
+  { path: 'stopwatch', component: StopwatchComponent},
+  { path: 'others', component: OthersComponent}
 ];
 
 @NgModule({
