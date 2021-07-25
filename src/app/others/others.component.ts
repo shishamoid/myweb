fc1f32f4e192980506c4ea37ea9dad0421900e8f
itodaisuke <itodaisuke0812@gmail.com>
2021-07-26 04:03:59 +0900

    update vr and arduino movie

--- src/app/others/others.component.ts
+++ src/app/others/others.component.ts
@@ -6,19 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./others.component.css']
 })
 export class OthersComponent implements OnInit {
-  //treeControl = new NestedTreeControl(node => node.children);
-  panelOpenState = false;
-  panelOpenState1 = false;
-  panelOpenState2 = false;
-  panelOpenState3 = false;
-  panelOpenState4 = false;
-  panelOpenState5 = false;
-  panelOpenState6 = false;
-  panelOpenState7 = false;
-
   constructor() { }
-
   ngOnInit(): void {
   }
-
 }
